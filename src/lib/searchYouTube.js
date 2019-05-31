1657fec00392f4dcd340f84994d2e644891546fe
Therese Brown <browntm96@gmail.com>
2019-05-30 20:34:17 -0700

    Complete creating reusable API helper

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,16 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  var alt = {
+    part: 'snippet',
+    key: options.key,
+    q: options.query,
+    maxResults: options.max,
+    type: 'video',
+    videoEmbeddable: 'true'
+  };
+
+  $.get('https://www.googleapis.com/youtube/v3/search', alt)
+    .done(function(results) { callback(results.items); });
 };
 
 export default searchYouTube;
